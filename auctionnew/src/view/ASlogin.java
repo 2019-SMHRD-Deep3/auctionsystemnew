5ea30bffccd1cf2b1f24b057135bf62df20e0b93
User <User@DESKTOP-002N2V1>
2020-01-13 11:55:53 +0900

    add iamage

--- auctionnew/src/view/ASlogin.java
+++ auctionnew/src/view/ASlogin.java
@@ -1,12 +1,18 @@
 package view;
 
 import java.awt.EventQueue;
+import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
 
 public class ASlogin {
 
 	private JFrame frame;
+	private JPanel panel;
+	private ImageIcon icon;
 
 	/**
 	 * Launch the application.
@@ -36,8 +42,30 @@ public class ASlogin {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1179, 766);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
-
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		ImageIcon icon = new ImageIcon("C:\\Users\\User\\Documents\\카카오톡 받은 파일\\dddd.PNG");
+		
+		panel = new JPanel(){
+			protected void paintComponent(Graphics g) {
+				g.drawImage(icon.getImage(), 0, 0, panel.getWidth(), panel.getHeight(), null);
+	            setOpaque(false);
+	            super.paintComponent(g);
+			
+			}
+			
+			};
+		springLayout.putConstraint(SpringLayout.NORTH, panel, 100, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panel, 10, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, 367, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel, 1151, SpringLayout.WEST, frame.getContentPane());
+		frame.getContentPane().add(panel);
+		
+		
+		
+		
+		}
 }
