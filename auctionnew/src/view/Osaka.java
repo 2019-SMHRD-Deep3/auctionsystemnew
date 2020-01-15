a29d3e232b5a2b92bbe086f90f8a05a6d6213450
SM020 <SM020@DESKTOP-38I4B2P>
2020-01-15 16:48:19 +0900

    change

--- auctionnew/src/view/Osaka.java
+++ auctionnew/src/view/Osaka.java
@@ -6,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import javax.swing.JTextPane;
 
 public class Osaka {
 
@@ -31,6 +32,11 @@ public class Osaka {
 		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\SM020\\git\\auctionsystemnew1\\auctionnew\\img\\\uC624\uC0AC\uCE74.JPG"));
 		lblNewLabel.setBounds(12, 10, 463, 324);
 		frame.getContentPane().add(lblNewLabel);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setText("\uC557!!!! \uC774\uC2DC\uAD6D\uC5D0 \uC624\uC0AC\uCE74\uB97C???????????");
+		textPane.setBounds(487, 10, 315, 421);
+		frame.getContentPane().add(textPane);
 	}
 
 }
